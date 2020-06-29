db0c9a859f5f98ac09b938903fe372127ae8f2e2
RicardoBarra <rique_barra@hotmail.com>
2020-06-29 13:27:47 +0100

    Files react app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          I feel JS is great and React is awesome!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
